89abd00db5ce65a21d53ef1f2d9a8c81038f4205
“Clare <clare.e.prowse@gmail.com>
2016-02-24 16:04:30 -0500

    Adding files

--- dev/scripts/scripts.js
+++ dev/scripts/scripts.js
@@ -1,9 +1,9 @@
 // set up beer app
-var boozeOutput = {};
+var app = {};
 
-boozeOutput.apiKey = 'MDoxNjU2NzBjYy1kOWMyLTExZTUtODBhOS1jMzQ4YTY5Nzc5OTc6QVBsb0RDTzF6dE9FQ0F0UGNYQjBUM3BUb0xyUWFuRW1Mc29I';
+app.apiKey = 'MDoxNjU2NzBjYy1kOWMyLTExZTUtODBhOS1jMzQ4YTY5Nzc5OTc6QVBsb0RDTzF6dE9FQ0F0UGNYQjBUM3BUb0xyUWFuRW1Mc29I';
 
-boozeOutput.apiUrl = 'https://lcboapi.com/products';
+app.apiUrl = 'https://lcboapi.com/products';
 
 // get information from LCBO API
 boozeOutput.getInfo = function () {
